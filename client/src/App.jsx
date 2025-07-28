cf56a7303b75c22b44c381d860fc33e74be498fd
Abhinav Ghosh <ghoshab03@gmail.com>
2025-07-29 02:44:06 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button, Typography, Container } from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Quiz Platform Frontend 🎯
+      </Typography>
+      <Button variant="contained">Hello Material UI</Button>
+    </Container>
+  );
+}
+
+export default App;
